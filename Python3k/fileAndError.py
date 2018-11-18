180b66b6c9363bcbb2bb6fb5b1c73be63eb95c35
VYuLinLin <lylin0311@163.com>
2018-11-18 22:09:13 +0800

    Signed-off-by: VYuLinLin <lylin0311@163.com> add pip and jupyter module

--- Python3k/fileAndError.py
+++ Python3k/fileAndError.py
@@ -1,4 +1,6 @@
-# 写入文件
+import time
+
+# 写入模式
 def writefile(*str):
   with open('assets/pi_str.txt', 'w') as fileObj:
     print(str)
@@ -13,12 +15,32 @@ def writefile(*str):
 
 writefile()
 
-# 读取文件
+# 只读模式（默认模式）
 def readfile(path='assets/pi_str.txt'):
   with open(path) as notes:
     note = notes.read()
     # 不能直接打印notes.read(),可以打印赋值后的变量
-    print('notes.read() = ',notes.read())
+    print('notes.read() = ',notes.read())  # 空值
     print('notes.read()变量 = ',note)
+    print('notes.readline() = ',notes.readline())
+    print('notes.readlines([]) = ',notes.readlines())
     print('note.rstrip() = ', note.rstrip())
 
+# 读写模式
+def read_write():
+  with open('assets/rw.txt', 'r+') as obj:
+    c = obj.read()
+    if c:
+      print(c)
+    else:
+      obj.write(time.time())
+
+# 写读模式
+def write_read():
+  with open('assets/wr.txt', 'w+') as obj:
+    c = obj.read()
+    print(c, obj)
+    if c:
+      print(c)
+    else:
+      obj.write(str(time.time()))
